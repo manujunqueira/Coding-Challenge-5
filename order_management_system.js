ac38756a45044ef0521e1417d4b75e3c1580f0b0
manujunqueira <manuelajunqueira27@gmail.com>
2024-09-19 10:15:01 -0400

    Create an Inventory Array of Product Objects

---
+++ order_management_system.js
@@ -0,0 +1,10 @@
+// Task 1 - Create an Inventory Array of Product Objects
+
+const inventory = [
+    { name: `Latte`, price: 7, quantity: 4 },
+    { name: `Capuccino`, price: 6, quantity: 7 },
+    { name: `Macchiato`, price: 8, quantity: 8 },
+    { name: `Black Coffe`, price: 5, quantity: 10 }
+    ];
+
+    
